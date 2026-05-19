54fd1d5554cc984f17c9e4c5636bee87561b4533
Testing Name <122777153+swurpyy@users.noreply.github.com>
2026-05-19 21:21:02 +0200

    feat: add sign in index
    stylized Clerk sign-in card and added a starting animation

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -6,9 +6,27 @@ import { ClerkProvider } from "@clerk/clerk-react";
 
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
 
+const appLocalization = {
+  signIn: {
+    start: {
+      title: 'Welcome to AirWays'
+    },
+  },
+
+  signUp: {
+    start: {
+      title: 'Create an AirWays Account'
+
+    }
+  }
+}
+
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
+    <ClerkProvider
+      publishableKey={PUBLISHABLE_KEY}
+      localization={appLocalization}
+    >
       <App />
     </ClerkProvider>
   </StrictMode>,
