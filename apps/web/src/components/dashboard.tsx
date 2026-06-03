79f0187a11d63257e0a2377566976067756746ad
Testing Name <122777153+swurpyy@users.noreply.github.com>
2026-06-03 21:53:34 +0200

    feat: add plane map

--- apps/web/src/components/dashboard.tsx
+++ apps/web/src/components/dashboard.tsx
@@ -1,11 +1,12 @@
 import "../global.css"
 import { UserButton, useUser } from "@clerk/clerk-react"
+import PlaneMap from "./planeMap";
 
 export default function Dashboard(){
     const { user } = useUser();
 
     return(
-        <div className="flex flex-col min-h-screen bg-[url(/index-bg.jpg)] bg-cover bg-center">
+        <div className="flex flex-col h-screen w-screen overflow-hidden bg-[url(/index-bg.jpg)] bg-cover bg-center">
             <header className="w-full flex justify-between items-center bg-black/20 p-5 shadow-md">
                 <div>
                     <h1 className="text-2xl font-bold text-white drop-shadow-md">AirWay</h1>
@@ -15,11 +16,9 @@ export default function Dashboard(){
                     <UserButton/>
                 </div>
             </header>
-            <body>
-                <div>
-                    <p>test</p>
-                </div>
-            </body>
+            <div className="grow w-full">
+                <PlaneMap/>
+            </div>
             <footer></footer>
         </div>
     );
